3f461e734805abb8e6c85960a12d2757e35eb4d8
react117 <avikbhattacharyya.2k@gmail.com>
2019-10-06 23:22:45 +0530

    [FEATURE] Added support for error logging

--- network_monitor.py
+++ network_monitor.py
@@ -6,8 +6,13 @@
 import socket
 import threading
 import queue
+import logging
+from datetime import datetime
 
 class NetworkMonitor(threading.Thread):
+    # initialize the log settings
+    logging.basicConfig(filename='log/' + datetime.now().strftime("%Y%m%d") + '_error.log', format='[%(asctime)s.%(msecs)03d]: %(name)s - %(levelname)s - %(message)s')
+
     def __init__(self, host_address='www.google.com', port=80, interval=2):
         self.host_address = host_address
         self.port = port
@@ -22,11 +27,12 @@ class NetworkMonitor(threading.Thread):
                 host_ip = socket.gethostbyname(self.host_address)
                 s.connect((host_ip, self.port))
                 self.op_queue.put('Network available')
-            except:
+            except IOError as e:
                 self.op_queue.put('Network not available')
+                logging.error('There is an error in connection. Error : ' + str(e))
         except socket.error as msg:
             self.op_queue.put('Network not available')
-            # print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            logging.error('There is an error in socket creation. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 
         print(self.op_queue.get())
         s.close()
